65537ef9e74b9c5a88470b37ef44df4c7dd8ad2a
mscott1995 <42160582+mscott1995@users.noreply.github.com>
2020-03-25 20:17:38 -0400

    Update restartserver.bat

--- restartserver.bat
+++ restartserver.bat
@@ -50,9 +50,9 @@ SET TESTCPUAFFINITY=40
 
 :: ****************************** MODS ******************************
 :: MODID look in meta.cpp. Use comma to add but no comma at the end
-SET MODIDS=1559212036,1564026768,1574054508,1576057168,1590841260,1710977250,1724005476,1623711988,1928939749,1585487785,1927567004,1782854982
+SET MODIDS=1559212036,1564026768,1574054508,1576057168,1590841260,1710977250,1724005476,1623711988,1928939749,1585487785,1927567004,1782854982,1870524790
 :: Add the Mod Name WITH the @ symbol close it with the ; as usual
-SET MODNAMES=@CF;@Community-Online-Tools;@BuildAnywhere;@BaseBuildingLogs;@Trader;@BaseBuildingPlus;@Durable Supressors;@VanillaPlusPlusMap;@Ear-Plugs;@Catch_Rain;@DayZ-Expansion-Notifications;@DayZ-Expansion-Chat;@No Shoe Damage
+SET MODNAMES=@CF;@Community-Online-Tools;@BuildAnywhere;@BaseBuildingLogs;@Trader;@BaseBuildingPlus;@Durable Supressors;@VanillaPlusPlusMap;@Ear-Plugs;@Catch_Rain;@DayZ-Expansion-Notifications;@DayZ-Expansion-Chat;@No Shoe Damage;@Airdrop-Upgraded
 :: Add the ModID in the [] and Mod Name WITHOUT the @ symbol
 SET MOD[1559212036]=CF
 SET MOD[1564026768]=Community-Online-Tools
@@ -67,6 +67,7 @@ SET MOD[1928939749]=Catch_Rain
 SET MOD[1585487785]=DayZ-Expansion-Notifications
 SET MOD[1927567004]=DayZ-Expansion-Chat
 SET MOD[1782854982]=No Shoe Damage
+SET MOD[1870524790]=Airdrop-Upgraded
 :: ********************************************************************************
 :: **Do not change anything below this comment unless you know what you are doing**
 :: ********************************************************************************
